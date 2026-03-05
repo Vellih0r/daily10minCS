28bfd35b2a201806a9ea255f6b1e0e1221b7109d
Vellih0r <yegrashich@gmail.com>
2026-03-05 11:23:53 +0000

    beta - adding el to the hashmap

--- c/data_structures/hashmap.c
+++ c/data_structures/hashmap.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "l_list.h"
 
 
@@ -7,22 +8,35 @@ int hash(int val)
     return val*15;
 }
 
-typedef struct hashmap
+typedef struct Hashmap
 {
-    int key;
-    struct Node* list;
+    int size;
+    struct Node** list;
 }Hashmap;
+
+void init(Hashmap* h)
+{
+   h->size = 5;
+   h->list = malloc(sizeof(struct Node*) * h->size); 
+}
+
+void push(Hashmap* map, int key, int value)
+{
+    int index = hash(key);
+    index = index % map->size;
+    l_add(&(map->list[index]), value);
+}
 
 int main()
 {
-
-    struct Node *head = { NULL };
     Hashmap test;
-    test.key = 'a';
-    test.list = head;
-    l_add(&test.list, 5);
-    l_add(&test.list, 5);
-    l_print(&test.list);
+    init(&test);
+    push(&test, 'a', 5);
+    int i = hash('a');
+    i = i % 5;
+    printf("%d\n", i);
+    l_add(&test.list[i], 10);
+    l_print(&test.list[i]);
     int a = 75;
     printf("val %d\n", a);
     int h = hash(a);
