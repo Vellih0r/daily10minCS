e22d12f8121333272a0cc80f0e5ed31cb74b101c
Vellih0r <yegrashich@gmail.com>
2026-03-07 14:24:36 +0000

    gpt reviewed

--- py_solo/encrypt/caesar.py
+++ py_solo/encrypt/caesar.py
@@ -1,6 +1,6 @@
 SECRET_KEY = 5
 # simple_alphabet=" abcdefghijklmnopqrstuvwxyz"
-alphabet=''' ABCDEFGHIJCLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz/`_~|[]{}.,?*&^%$#@!()\'<>\\"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхчшщъыьэюя0123456789'''
+alphabet=''' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz/`_~|[]{}.,?*&^%$#@!()\'<>\\"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхчшщъыьэюя0123456789'''
 
 # Character TO Integer
 # create dict - character : integer
@@ -12,9 +12,8 @@ for index, char in enumerate(alphabet):
 # itoc = Integer TO Char -> we can get chracter by its index, using regular list
 # list[1] = a
 # list[26] = z
-itoc = []
-for char in alphabet:
-    itoc.append(char)
+# lsit() - converts string to array
+itoc = list(alphabet)
 
 # constant for alpphabet's length
 LENGTH = len(itoc)
@@ -26,39 +25,72 @@ def encrypt(msg: str, shift: int) -> str:
     
     returns new encripted message
     '''
+    # normolize shift, if it is way to large
+    if shift > LENGTH:
+        shift = shift % LENGTH
     # result will be saved here
     out = ""
 
     # get each character from message
     for c in msg:
-        # ctoi.get() will return None if there is no such char
         index = ctoi.get(c)
-        # check if index is not None
-        if index is not None:
-            # if index is in bounds
-            if index+shift < LENGTH:
-                index += shift # simply shift it forward
-            else:
-                # if index is more than END (z)
-                # we must go back to the START
-                steps_to_end = LENGTH - index
-                index = 0 + (shift - steps_to_end)
-            # add new shifted index to the result
-            out += itoc[index];
+        # check if index is None (is it supported?, if no - add it straight to result, do not encrypt it)
+        if index is None:
+            out += c
+            # skip to the next iteration
+            continue
+
+        # if index is in bounds
+        if index+shift < LENGTH:
+            index += shift # simply shift it forward
+        else:
+            # if index is more than END (z)
+            # we must go back to the START
+            steps_to_end = LENGTH - index
+            index = 0 + (shift - steps_to_end)
+        # add new shifted index to the result
+        out += itoc[index];
     # return result after for loop
     return out
 
 
 def decrypt(msg: str, shift: int) -> str:
+    '''
+    decrypt message using key
+    returns decrypted message - str
+    '''
+    # normolize shift, if it is way to large
+    if shift > LENGTH:
+        shift = shift % LENGTH
+
     out = ""
     for c in msg:
         index = ctoi.get(c)
-        if index is not None:
-            if index-shift >= 0:
-                index -= shift
-            else:
-                index = LENGTH - (shift - index)
-            out += itoc[index]
+        if index is None:
+            out += c
+            continue
+
+        if index-shift >= 0:
+            index -= shift
+        else:
+            index = LENGTH - (shift - index)
+        out += itoc[index]
+    return out
+
+def gtp_decrypt(msg: str, shift: int) -> str:
+    shift %= LENGTH
+    out = ""
+
+    for c in msg:
+        index = ctoi.get(c)
+
+        if index is None:
+            out += c
+            continue
+
+        # loop around
+        index = (index + shift) % LENGTH
+        out += itoc[index]
     return out
 
 
