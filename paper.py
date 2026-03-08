6c980c12b12b89d86a6d2495a5d69eda13671db0
Vellih0r <yegrashich@gmail.com>
2026-03-08 12:55:22 +0000

    nothing

--- paper.py
+++ paper.py
@@ -1,20 +1,9 @@
-from PIL import ImageGrab
 import time
-image = ImageGrab.grab()
-color = image.getpixel((200,200))
-print(color)
 
+start = time.time()
+list(map(lambda x: x*x, range(1, 10001)))
+print("map:", time.time() - start)
 
-
-# Source - https://stackoverflow.com/a/15994043
-# Posted by Ed Shway, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-03-04, License - CC BY-SA 3.0
-
-# from pymouse import PyMouse
-
-# m = PyMouse()
-# m.position() #gets mouse current position coordinates
-# m.move(x,y)
-# m.click(x,y) #the third argument "1" represents the mouse button
-# m.press(x,y) #mouse button press
-# m.release(x,y) #mouse button release
+start = time.time()
+[x*x for x in range(1, 10001)]
+print("list comp:", time.time() - start)
